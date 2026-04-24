a69ebb133dba8b9d528c91b3cc668497b5babf1f
이정민 <protain02@naver.com>
2026-04-24 10:27:00 +0900

    시간포멧추가

---
+++ src/main/java/com/tenco/blog/model/Board.java
@@ -0,0 +1,30 @@
+package com.tenco.blog.model;
+
+import com.tenco.blog.util.MyDateUtil;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.Timestamp;
+
+@Data // get, set, toString ..
+// @Entity - JPA가 이 클래스를 데이터베이스 테이블과 매핑하는 객체로 인식하게 설정
+// 즉, 이 어노테이션이 있어야 JPA가 관리 함
+@Entity
+@Table(name = "board_tb")
+public class Board {
+    // @Id: 이 필드가 기본키임을 설정 함
+    @Id
+    // IDENTITY 전략: 데이터베이스의 기본 AUTO_INCREMENT 기능 사용
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String title;
+    private String content;
+    private Timestamp createdAt;
+
+    // createdAt -> 포멧하는 메서드 만들어 보기
+    public String getTime() {
+        return MyDateUtil.timestampFormat(createdAt);
+    }
+}
